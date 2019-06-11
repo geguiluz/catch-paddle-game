fb5fab819f4b2ccbd347c27bda8aca0b96d36226
geguiluz <memo.eguiluz@gmail.com>
2019-06-11 15:01:50 -0500

    Front-end logic MVP ready to be connected to game logic through updateGameSummary()

--- src/front-end-logic.js
+++ src/front-end-logic.js
@@ -1,10 +1,10 @@
 
 Vue.component('green-square',{
-      props: ['bar'],
-      template: '<div class = "catch-count-bar bar-bezel"><div class="green-square"></div><div class="green-square"></div><div class="green-square"></div><div class="green-square"></div><div class="green-square"></div><div class="green-square"></div></div>'
+      props: ['squares'],
+      template: '<div class="green-square"></div>'
     });
 Vue.component('strike-square',{
-    props: ['bar'],
+    props: ['squares'],
     template: '<div class="strike-square">&#9760;</div>'
     });
 
@@ -13,25 +13,52 @@ new Vue({
     data: {
       gameSummary: {
         diffLevel: 1,
-		catchCount: 6,
+        catchCount: 6,
+        catchArray: [1, 2, 3, 4, 5, 6],
         strikesLeft: 4,
+        strikesArray: [1, 2, 3, 4],
         messageBar: [
             {
-                message: "Good catch",
+                // {{ gameSummary.messageBar[0].message }}
+                message: "Good catch!",
                 success: true
             },
             {
                 message: "Oops",
-                success: true
+                success: false
             }
         ]
       }
     },
     methods: {
-        showGameInformation: function(diffLevel, catchCount, strikesLeft){
-          this.diffLevel = diffLevel;
-          this.catchCount = catchCount;
-          this.strikesLeft = strikesLeft;
+        updateGameSummary: function(diffLevel, catchCount, strikesLeft){
+          this.diffLevel = game.diffLevel;
+          this.catchCount = game.catchCount;
+          this.strikesLeft = game.strikesLeft;
+          var catchDelta = game.catchCount - gameSummary.catchArray.length;
+          var strikesDelta = game.strikesLeft - gameSummary.strikesArray.length;
+          matchBarArrays(catchDelta, 'catch-bar');
+          matchBarArrays(strikesDelta, 'strikes-bar');
+        },
+        matchBarArrays(delta, barType){
+            if (delta < 0) {
+                for (i = 0; i < Math.abs(delta); i++){
+                    // Remove array elements until quantities match
+                    if (barType === 'catch-bar'){
+                        gameSummary.catchArray.pop();
+                    } else {
+                        gameSummary.strikesArray.pop();
+                    }
+                }
+              } else if (game.catchCount > gameSummary.catchArray.length){
+                for (i = 0; i < Math.abs(delta); i++){
+                    // Add array elements until quantities match
+                    if (barType === 'catch-bar'){
+                        gameSummary.catchArray.push('x');
+                    } else {
+                        gameSummary.strikesArray.puxh('x');
+                    }                }
+              }
         }
     }
 });
